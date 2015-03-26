6d6efd440c76ffac3fb6b845fab521744ad721a9
Fomina Natal`ya <fna9@ice23.msiu.ru>
2015-03-26 12:21:52 +0300

    homework26

--- app/helpers/application_helper.rb
+++
@@ -1,5 +0,0 @@
-module ApplicationHelper
-  def error_messages_for(object)
-    render "error_messages", object: object
-  end
-end
--- app/models/product.rb
+++
@@ -1,5 +0,0 @@
-class Product < ActiveRecord::Base
-  validates :name, presence: true, length: {maximum: 200}
-  validates :price, presence: true, numericality: {greater_than: 0}
-  validates :weight, presence: true, numericality: {only_integer: true, greater_than: 0}, allow_blank: true
-end
