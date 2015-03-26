d320b41ad09f87ca0913358abaf13b7e6edfcb8c
Fomina Natal`ya <fna9@ice23.msiu.ru>
2015-03-26 12:28:15 +0300

    home2

---
+++ app/models/product.rb
@@ -0,0 +1,14 @@
+class Product < ActiveRecord::Base
+  has_many :line_items, dependent: :nullify
+  validates :name, presence: true, length: {maximum: 200}
+  validates :price, presence: true, numericality: {greater_than: 0}
+  validates :weight, numericality: {only_integer: true, greater_than: 0}, allow_blank: true
+  before_destroy :can_destroy?
+
+
+
+
+  def can_destroy?
+    line_items.blank?
+  end
+end
