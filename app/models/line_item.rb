21d1e05feecc2c282d5ef6fe6287cb35ac8ead15
Fomina Natal`ya <fna9@ice12.msiu.ru>
2015-04-09 11:28:09 +0300

    delete

--- app/models/line_item.rb
+++
@@ -1,9 +0,0 @@
-class LineItem < ActiveRecord::Base
-  validates :product, presence: true
-  validates :product_id, uniqueness: {scope: :cart_id}
-  validates :cart, presence: true
-  validates :quanitem,presence: true,numericality: {only_integer: true,order_than:0}
-  validates :price, presence: true
-  belongs_to :cart
-  belongs_to :product
-end
--- app/views/line_items/show.json.jbuilder
+++
@@ -1,1 +0,0 @@
-json.extract! @line_item, :id, :quanitem, :price, :cart_id, :product_id, :created_at, :updated_at
